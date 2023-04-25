6090c3c079b18751fc51b44831dfe13201952917
Vinicios-Developer <109308457+Vinicios-Developer@users.noreply.github.com>
2023-04-25 03:54:00 -0400

    atualização

--- main.py
+++ main.py
@@ -1,40 +1,104 @@
-import kivy
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
 from plyer import gps
-from plyer import notification
+from kivy.app import App
+from kivy.properties import StringProperty
+from kivy.clock import mainthread
+from kivy.utils import platform
 
-kivy.require("1.11.1")
+kv = '''
+BoxLayout:
+    orientation: 'vertical'
+
+    Label:
+        text: app.gps_location
+
+    Label:
+        text: app.gps_status
+
+    BoxLayout:
+        size_hint_y: None
+        height: '48dp'
+        padding: '4dp'
+
+        ToggleButton:
+            text: 'Start' if self.state == 'normal' else 'Stop'
+            on_state:
+                app.start(1000, 0) if self.state == 'down' else \
+                app.stop()
+'''
 
 
-class GPSApp(App):
+class GpsTest(App):
+
+    gps_location = StringProperty()
+    gps_status = StringProperty('Click Start to get GPS location updates')
+
+    def request_android_permissions(self):
+        """
+        Since API 23, Android requires permission to be requested at runtime.
+        This function requests permission and handles the response via a
+        callback.
+
+        The request will produce a popup if permissions have not already been
+        been granted, otherwise it will do nothing.
+        """
+        from android.permissions import request_permissions, Permission
+
+        def callback(permissions, results):
+            """
+            Defines the callback to be fired when runtime permission
+            has been granted or denied. This is not strictly required,
+            but added for the sake of completeness.
+            """
+            if all([res for res in results]):
+                print("callback. All permissions granted.")
+            else:
+                print("callback. Some permissions refused.")
+
+        request_permissions([Permission.ACCESS_COARSE_LOCATION,
+                             Permission.ACCESS_FINE_LOCATION], callback)
+        # # To request permissions without a callback, do:
+        # request_permissions([Permission.ACCESS_COARSE_LOCATION,
+        #                      Permission.ACCESS_FINE_LOCATION])
 
     def build(self):
-        self.layout = BoxLayout(orientation="vertical")
-        self.button = Button(text="Obter Localização", size_hint_y=None, height=50)
-        self.button.bind(on_press=self.get_location)
-        self.layout.add_widget(self.button)
-        return self.layout
-
-    def get_location(self, obj):
         try:
-            gps.configure(on_location=self.on_location)
-            gps.start()
+            gps.configure(on_location=self.on_location,
+                          on_status=self.on_status)
         except NotImplementedError:
-            notification.notify(
-                title="GPS",
-                message="GPS não disponível neste dispositivo.",
-            )
+            import traceback
+            traceback.print_exc()
+            self.gps_status = 'GPS is not implemented for your platform'
 
+        if platform == "android":
+            print("gps.py: Android detected. Requesting permissions")
+            self.request_android_permissions()
+
+        return Builder.load_string(kv)
+
+    def start(self, minTime, minDistance):
+        gps.start(minTime, minDistance)
+
+    def stop(self):
+        gps.stop()
+
+    @mainthread
     def on_location(self, **kwargs):
-        lat = kwargs["lat"]
-        lon = kwargs["lon"]
-        notification.notify(
-            title="Localização",
-            message=f"Sua localização é: {lat}, {lon}",
-        )
+        self.gps_location = '\n'.join([
+            '{}={}'.format(k, v) for k, v in kwargs.items()])
+
+    @mainthread
+    def on_status(self, stype, status):
+        self.gps_status = 'type={}\n{}'.format(stype, status)
+
+    def on_pause(self):
+        gps.stop()
+        return True
+
+    def on_resume(self):
+        gps.start(1000, 0)
+        pass
 
 
-if __name__ == "__main__":
-    GPSApp().run()
+if __name__ == '__main__':
+    GpsTest().run()
